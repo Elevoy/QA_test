48e9cd6aebb4740ab347efb6fc65556df799eb04
Elena Voitulevych <elevoy@protonmail.com>
2021-09-04 16:39:45 +0300

    add bash_terminal.txt script1.sh

---
+++ script1.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+#
+cd doc.11
+mkdir doc11 doc12 doc13
+cd doc11
+touch test1.txt test2.txt test3.txt test1.json test2.json
+mkdir doc111 doc112 doc113
+ls -l
+mv tets1.txt test1.json Doc111/
+ 
+
